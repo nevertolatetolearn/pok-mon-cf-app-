02ea3d964741de30d4e5554b9f516ce549f0d601
nevertolatetolearn <theosphamilton@gmail.com>
2023-01-14 21:19:26 +0100

    scripts.js code updates
    If applied, this commit will create a new variable (pokemonList) and several data objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
- alert('Hello world');
+let pokemonList = [
+  {name: 'Sawk' , height: '1.4', type:['fighting','huamn-like','karate']},
+  {name: 'Togepi' , height: '.3', type:['fairy','no-eggs', 'normal']},
+  {name: 'Togetic' , height: '.6', type:['fairy','flying','normal']},
+  {name: 'Togekiss' , height: '1.5', type:['fairy','flying','normal']},
+  {name: 'Charmander' , height: '.6', type:['monster','dragon','fire']}
+  {name: 'Charmeleon' , height: '1.1', type:['monster','dragon','fire']}
+  {name: 'Charizard' , height: '1.7', type:['monster','dragon','fire','flying']},
+  {name: 'Blitzle' , height: '.8', type:['electric','horse','speed','thunder']},
+  {name: 'Zebstrika' , height: '1.6', type:['electric','horse','speed','thunder']}
 
- let favoriteFood = 'Jerk Chicken';
- document.write(favoriteFood);
+];
