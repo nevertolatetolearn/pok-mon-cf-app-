55a3a2c96547d9127bffeba2d811dc77ee33a651
nevertolatetolearn <theosphamilton@gmail.com>
2023-02-08 21:02:42 +0100

    Update For Loop
    If applied, this commit will have updated the for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,21 @@
 let pokemonList = [
-  {name: 'Sawk' , height: '1.4', type:['fighting','huamn-like','karate']},
-  {name: 'Togepi' , height: '.3', type:['fairy','no-eggs', 'normal']},
-  {name: 'Togetic' , height: '.6', type:['fairy','flying','normal']},
-  {name: 'Togekiss' , height: '1.5', type:['fairy','flying','normal']},
-  {name: 'Charmander' , height: '.6', type:['monster','dragon','fire']},
-  {name: 'Charmeleon' , height: '1.1', type:['monster','dragon','fire']},
-  {name: 'Charizard' , height: '1.7', type:['monster','dragon','fire','flying']},
-  {name: 'Blitzle' , height: '.8', type:['electric','horse','speed','thunder']},
-  {name: 'Zebstrika' , height: '1.6', type:['electric','horse','speed','thunder']}
+  {name: 'Sawk' , height: 1.4, type:['fighting','huamn-like','karate']},
+  {name: 'Togepi' , height: .3, type:['fairy','no-eggs', 'normal']},
+  {name: 'Togetic' , height: .6, type:['fairy','flying','normal']},
+  {name: 'Togekiss' , height: 1.5, type:['fairy','flying','normal']},
+  {name: 'Charmander' , height: .6, type:['monster','dragon','fire']},
+  {name: 'Charmeleon' , height: 1.1, type:['monster','dragon','fire']},
+  {name: 'Charizard' , height: 1.7, type:['monster','dragon','fire','flying']},
+  {name: 'Blitzle' , height: .8, type:['electric','horse','speed','thunder']},
+  {name: 'Zebstrika' , height: 1.6, type:['electric','horse','speed','thunder']},
 
 ];
 
-// For loop: to check the different Pokemons heights and 
+// For loop: to check the different Pokemons heights and
 //print out the correct information relating to the right conditions.
 for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >= 1.5) {
-      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - Wow, that is a big pokemon!" + "<br>");
-    } else if (pokemonList[i].height >= 1.0 && pokemonList[i].height < 5) {
-      document.write(pokemonList[i].name + " (height: " +pokemonList[i].height + "m) - That is a medium pokemon!" + "<br>");
-    } else {
-      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That is a small pokemon!" + "<br>");
-    }
-  }
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+     if (pokemonList[i].height >= 1.7) {
+      document.write("Wow, that's big!!" + "<br>");
+     }
+   }
